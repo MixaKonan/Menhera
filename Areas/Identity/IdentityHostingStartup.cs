d1c391adaf37d3688c1f4624b6bc13271f878d7f
Mikhaylo Konontsev <fater181@gmail.com>
2021-03-22 15:28:29 +0200

    Identity added

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Menhera.Areas.Identity.Data;
+using Menhera.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Menhera.Areas.Identity.IdentityHostingStartup))]
+namespace Menhera.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MenheraContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MenheraContextConnection")));
+
+                services.AddDefaultIdentity<MenheraUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MenheraContext>();
+            });
+        }
+    }
+}
