8f25376f281f1806f86c5942a7aa8d97c1d6bad0
Mykhailo Konontsev <fater181@gmail.com>
2021-03-24 19:01:55 +0200

    Gross hack because Ryan doesn't know how to code

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,21 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
 
 namespace Menhera.Models
 {
-    [Table("admin")]
-    public class Admin
+    public partial class Admin
     {
-        [Key]
-        [Column("admin_id")]
-        public int AdminId { get; set; }
-        
-        [Column("user_id")]
-        public int UserId { get; set; }
-        
-        [Column("is_superadmin")]
-        public bool IsSuperadmin { get; set; }
+        public Admin()
+        {
+            Ban = new HashSet<Ban>();
+        }
 
+        public int AdminId { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public bool CanDeletePosts { get; set; }
+        public bool CanCloseThreads { get; set; }
+        public bool HasAccessToPanel { get; set; }
+        public bool CanBanUsers { get; set; }
+        public string AdminIpHash { get; set; }
+
+        public virtual ICollection<Ban> Ban { get; set; }
     }
-        
 }
