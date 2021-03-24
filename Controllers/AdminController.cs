a0723bee904bb8fefb7ec2f2133ed764dd37dd98
Mikhaylo Konontsev <fater181@gmail.com>
2021-03-24 23:35:58 +0200

    Best commit ever

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Menhera.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Menhera.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         // GET
         public IActionResult Panel()
+        {
+            return View();
+        }
+
+        public IActionResult Create(BoardAdmin boardAdmin)
+        {
+            return View();
+        }
+        
+        public IActionResult Admins()
+        {
+            return View();
+        }
+        
+        public IActionResult Boards()
         {
             return View();
         }
---
+++ Models/BoardAdmin.cs
@@ -0,0 +1,8 @@
+﻿namespace Menhera.Models
+{
+    public class BoardAdmin
+    {
+        public Admin Admin { get; set; }
+        public Board Board { get; set; }
+    }
+}
