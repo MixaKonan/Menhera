147441435b5fa4b1c793d5e46bae193a748f4b35
Mykhailo Konontsev <fater181@gmail.com>
2021-04-08 19:43:29 +0300

    some brief changes

---
+++ wwwroot/js/thread/removeThread.js
@@ -0,0 +1,32 @@
+﻿const deleteButtons = document.querySelectorAll(".delete-thread");
+
+deleteButtons.forEach(item => item.addEventListener('click',
+    () => removeThread(item.id)));
+
+function removeThread(buttonId) {
+    let id = "";
+
+    for (let i = 3; i < buttonId.length; i++) {
+        id += buttonId[i];
+    }
+
+    let data = {threadId: id};
+
+    let confirmed = window.confirm("Удалить тред?");
+
+    if (confirmed) {
+        $.ajax({
+            url: "/Admin/RemoveThread",
+            contentType: 'application/x-www-form-urlencoded',
+            data: data,
+            method: 'post',
+            success: () => {
+                alert("Тред удалён.");
+            },
+            error: () => {
+                alert("Произошла ошибка.")
+            }
+        })
+    }
+}
+
