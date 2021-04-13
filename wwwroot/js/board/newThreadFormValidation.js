45dde93a082c06a2a1c3b226662242bb4c7eb0a8
Mykhailo Konontsev <fater181@gmail.com>
2021-04-13 21:09:45 +0300

    thread adding validation

--- wwwroot/js/board/newThreadFormValidation.js
+++ wwwroot/js/board/newThreadFormValidation.js
@@ -1,6 +1,6 @@
 ﻿const fileInput = document.getElementById("files");
 const submit = document.getElementById("submit");
-const textarea = document.getElementById("Comment")
+const textarea = document.getElementById("сomment")
 
 const threadLimit = 20;
 const commentLimit = 5000;
@@ -17,11 +17,27 @@ function validateNewThreadForm() {
     }
 
     let limit = document.getElementById("fileLimit").value
+    let files = fileInput.files;
 
-    if (fileInput.files.length > limit) {
+    if (files.length > limit) {
         window.alert("Файлов больше, чем разрешено на доске.");
         return false;
     }
+
+    for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+
+        if(!_allowedMimeFileExtensions.includes(file.type)) {
+            window.alert("Неподдерживаемый формат файла: " + file.type + "\nПоддерживаемые форматы: " + _allowedMimeFileExtensions.join(', '));
+            return false;
+        }
+
+        if(file.size > _allowedFileSize){
+            window.alert("Размер файла слишком большой. Разрешённый размер -- 5 МБ");
+            return false;
+        }
+    }
+
     return true;
 }
 
