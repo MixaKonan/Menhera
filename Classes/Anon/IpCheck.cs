3f52ae3a9bf00e1c3cd0bd750dd06643b9c0a844
Mykhailo Konontsev <fater181@gmail.com>
2021-04-15 15:01:31 +0300

    ip belongs to admin no longer needed

--- Classes/Anon/IpCheck.cs
+++ Classes/Anon/IpCheck.cs
@@ -8,21 +8,6 @@ namespace Menhera.Classes.Anon
 {
     public static class IpCheck
     {
-        public static bool IpBelongsToAdmin(string ipHash, IEnumerable<Admin> admins, out Admin admin)
-        {
-            admin = new Admin();
-            
-            foreach (var _admin in admins)
-            {
-                if (ipHash == _admin.AdminIpHash)
-                {
-                    admin = _admin;
-                    return true;
-                }
-            }
-            return false;
-        }
-
         public static bool UserIsBanned(MenherachanContext db, string ipHash)
         {
             try
