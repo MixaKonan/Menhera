098d8b0d9a7cc124c89a197aba748f6b9dbb5fc6
Mykhailo Konontsev <fater181@gmail.com>
2021-04-17 20:10:20 +0300

    gif info class

---
+++ Classes/Files/GifInformation.cs
@@ -0,0 +1,10 @@
+﻿namespace Menhera.Classes.Files
+{
+    public class GifInformation : FileInformation
+    {
+        public GifInformation(string fileName, string thumbnailName, string info) : base(fileName, thumbnailName, info)
+        {
+            
+        }
+    }
+}
